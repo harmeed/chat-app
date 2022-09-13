e2a9957bf6df8d3a282d7a6cb14c2c88a40f1f6f
hammed <hammedolalekan60@gmail.com>
2022-09-13 14:08:31 +0100

    Chores : Added home page

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const socket = require('socket.io');
+const express = require("express");
+const socket = require("socket.io");
 
 const app = express();
 
 const server = app.listen(2800, function () {
-    console.log('server listening on port 2800,');
+  console.log("server listening on port 2800,");
 });
 
-app.use(express.static('public')); 
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+app.use(express.static("public"));
 
 const io = socket(server);
 
-io.on('connection', (socket) => {
-    console.log('made socket connection',socket.id);
+io.on("connection", (socket) => {
+  console.log("made socket connection", socket.id);
 
-    socket.on('chat',function(data){
-        io.sockets.emit('chat', data);
-    });
-    socket.on('typing',function(data){
-        socket.broadcast.emit('typing',data);
-    });
+  socket.on("chat", function (data) {
+    io.sockets.emit("chat", data);
+  });
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
 });
